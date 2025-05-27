6addb28b2de536877439a8ad0f5a2cf4a5181a1e
trh4ckn0n <trhacknon@proton.me>
2025-05-27 02:24:56 +0200

    Create shorten.php

---
+++ shorten.php
@@ -0,0 +1,9 @@
+<?php
+$db = new SQLite3('data.db');
+$url = $_POST['url'];
+$password = $_POST['password'] ?? '';
+$code = substr(md5($url . time()), 0, 6);
+$db->exec("INSERT INTO urls (code, url, password) VALUES ('$code', '$url', '$password')");
+echo "<p style='color:#0f0;'>URL raccourcie : <a style='color:#0ff;' href='/$code'>https://yourdomain.com/$code</a></p>";
+?>
+<a href="index.php" style="color:#0f0;">Retour</a>
