5ada6d7c5f6f8d1b8174ea190bf54e7720ff1f3f
trh4ckn0n <trhacknon@proton.me>
2025-05-27 07:25:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-php /var/www/html/init_db.php
+
+# Créer la DB uniquement si elle n'existe pas
+if [ ! -f /var/www/html/urls.db ]; then
+    php /var/www/html/init_db.php
+    chown www-data:www-data /var/www/html/urls.db
+fi
+
 exec apache2-foreground
