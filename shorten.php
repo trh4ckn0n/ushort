8406c1e48c1b822cc0a53de0f8c64f013381b94b
trh4ckn0n <trhacknon@proton.me>
2025-05-27 02:40:19 +0200

    Update shorten.php

--- shorten.php
+++ shorten.php
@@ -4,6 +4,6 @@ $url = $_POST['url'];
 $password = $_POST['password'] ?? '';
 $code = substr(md5($url . time()), 0, 6);
 $db->exec("INSERT INTO urls (code, url, password) VALUES ('$code', '$url', '$password')");
-echo "<p style='color:#0f0;'>URL raccourcie : <a style='color:#0ff;' href='/$code'>https://yourdomain.com/$code</a></p>";
+echo "<p style='color:#0f0;'>URL raccourcie : <a style='color:#0ff;' href='/$code'>https://ushort-b3uz.onrender.com/$code</a></p>";
 ?>
 <a href="index.php" style="color:#0f0;">Retour</a>
